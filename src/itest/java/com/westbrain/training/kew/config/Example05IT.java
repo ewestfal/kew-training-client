d39919a3dee4f31d475ffaec7266079333c756e6
Eric Westfall <ewestfal@gmail.com>
2015-12-05 01:42:45 -0500

    Committing final version of kew api exercise

--- src/itest/java/com/westbrain/training/kew/config/Example05IT.java
+++ src/itest/java/com/westbrain/training/kew/config/Example05IT.java
@@ -1,9 +1,15 @@
 package com.westbrain.training.kew.config;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kuali.rice.kew.api.WorkflowDocument;
 import org.kuali.rice.kew.api.WorkflowDocumentFactory;
+import org.kuali.rice.kew.api.document.DocumentStatus;
+import org.kuali.rice.kew.framework.postprocessor.DocumentRouteStatusChange;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -35,6 +41,20 @@ public class Example05IT {
 		//     Initiated -> Enroute -> Approved -> Processed -> Final
 		// Status codes can be found using the getCode() method on org.kuali.rice.kew.api.document.DocumentStatus enumeration.	
 		
+		List<DocumentRouteStatusChange> routeStatusChanges = TrackingPostProcessor.getRouteStatusChanges();
+		assertEquals("There should have been 3 status changes.", 3, routeStatusChanges.size());
+		
+		DocumentRouteStatusChange change1 = routeStatusChanges.get(0);
+		assertEquals(DocumentStatus.INITIATED.getCode(), change1.getOldRouteStatus());
+		assertEquals(DocumentStatus.ENROUTE.getCode(), change1.getNewRouteStatus());
+		
+		DocumentRouteStatusChange change2 = routeStatusChanges.get(1);
+		assertEquals(DocumentStatus.ENROUTE.getCode(), change2.getOldRouteStatus());
+		assertEquals(DocumentStatus.PROCESSED.getCode(), change2.getNewRouteStatus());
+
+		DocumentRouteStatusChange change3 = routeStatusChanges.get(2);
+		assertEquals(DocumentStatus.PROCESSED.getCode(), change3.getOldRouteStatus());
+		assertEquals(DocumentStatus.FINAL.getCode(), change3.getNewRouteStatus());
 		
 	}
 	
